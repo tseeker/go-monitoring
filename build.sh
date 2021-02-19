ffe98f702b0674c4457c343b80e9d965420e1fed
Emmanuel Benoît <tseeker@nocternity.net>
2021-02-19 13:55:27 +0100

    Build script - Build for x86 and amd64

--- build.sh
+++ build.sh
@@ -2,10 +2,13 @@
 
 set -e
 cd "$(dirname $0)"
-mkdir -p bin
-for d in $(find cmd -mindepth 1 -maxdepth 1 -type d); do
-	cd $d
-	xn="$(basename "$d")"
-	go build
-	/bin/mv "$xn" ../../bin
+for arch in 386 amd64; do
+	mkdir -p bin/$arch
+	for d in $(find cmd -mindepth 1 -maxdepth 1 -type d); do
+		pushd $d >/dev/null
+		xn="$(basename "$d")"
+		GOARCH=$arch go build
+		/bin/mv "$xn" ../../bin/$arch
+		popd >/dev/null
+	done
 done
